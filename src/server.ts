e895727733659fe77ab0305a71714caa1098fdce
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2026-05-15 14:32:29 +0200

    environment setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 import { routeHandler } from "./routes/routes";
+import config from "./config";
 
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
   routeHandler(req, res);
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on the port 3000");
+server.listen(config.port, () => {
+  console.log(`Server is running on the port ${config.port}`);
 });
